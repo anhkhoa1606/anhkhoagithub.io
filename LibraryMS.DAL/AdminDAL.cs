cdd6572ba875213b6d1f8558ad2d46a21c78797c
anhkhoa1606 <120777923+anhkhoa1606@users.noreply.github.com>
2022-12-23 13:25:06 +0700

    first commit

---
+++ LibraryMS.DAL/AdminDAL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using LibraryMS.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryMS.DAL
+{
+    public class AdminDAL
+    {
+        
+        //LIST USED TO STORE ADMINS DETAILS
+        private static List<Admin> admins = new List<Admin>()
+        {
+            new Admin()
+            {
+                AdminId=1,AdminName="anhkhoa",AdminEmail="anhkhoa123@gmail.com",AdminPassword="123123"
+            },
+        };
+        //RETURNING ADMIN FROM ADMIN TABLE =>DAL
+        public List<Admin> GetAllAdminsDAL()
+        {
+            return admins;
+        }
+    }
+}
